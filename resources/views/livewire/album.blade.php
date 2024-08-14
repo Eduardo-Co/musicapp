f7357b198536bb3e3284e4baf7890890f70450b1
Eduardo <gerencia.desenvolvimento@unimontes.br>
2024-08-14 09:42:11 -0300

    Fix: nome artista

--- resources/views/livewire/album.blade.php
+++ resources/views/livewire/album.blade.php
@@ -191,7 +191,7 @@
                             <label class="block text-sm font-medium text-gray-700">Artists</label>
                             <div class="flex flex-wrap gap-2 mt-2">
                                 <div class="bg-gray-100 px-4 py-2 rounded-lg shadow-sm">
-                                    <span>{{ $viewingAlbum->artista->nome }}</span>
+                                    <span>{{ $viewingAlbum->artistas->nome }}</span>
                                 </div>
                             </div>
                         </div>
